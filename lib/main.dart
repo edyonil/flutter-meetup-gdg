8f2c974885e11e371df29de1a8d445ed1375cb96
alexrsg <alexrsg@gmail.com>
2019-11-22 22:56:55 -0300

    State Default

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter_meetup/bloc/increment.bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,35 +44,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  final IncrementBloc _incrementBloc = IncrementBloc();
-
-  @override
-  void dispose(){
-    this._incrementBloc.dispose();
-    super.dispose();
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Resolvido o rebuild");
+    print("Vou refazer a build");
     return Scaffold(
-      appBar: AppBar(title: Text('Stream version of the Counter App')),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
-        child: StreamBuilder<int>(
-          stream: _incrementBloc.incrementValueStream,
-          initialData: 0,
-          builder: (BuildContext context, AsyncSnapshot<int> snapshot){
-            return Text('You hit me: ${snapshot.data} times');
-          }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: (){
-          this._incrementBloc.incrementeValue();
-        },
-      ),
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
